93349ad69a2ce67531de21b4156bf9fb1821ff05
Ronak <ronak@getpi.in>
2024-12-22 23:45:47 +0530

    Added experience, portfolio and new project section

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,6 +57,18 @@ skillsHeader.forEach((el) => {
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination:{
+        el: '.swiper-pagination',
+        clickable: true
+    }
+});
 
 
 /*==================== TESTIMONIAL ====================*/
